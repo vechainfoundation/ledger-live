7b655e97b7cca8fe956f5bc56aff9ce90796ff02
Mirko Genoni <mirko.genoni@mail.polimi.it>
2023-02-24 09:48:04 +0100

    implementation VTHO token

--- libs/ledger-live-common/src/families/vechain/js-synchronisation.ts
+++ libs/ledger-live-common/src/families/vechain/js-synchronisation.ts
@@ -3,8 +3,10 @@ import { BigNumber } from "bignumber.js";
 import { makeSync, makeScanAccounts, mergeOps } from "../../bridge/jsHelpers";
 import { encodeAccountId } from "../../account";
 import eip55 from "eip55";
+import { emptyHistoryCache } from "../../account";
 
 import { getAccount, getOperations, getTokenOperations } from "./api";
+import { getTokenById } from "@ledgerhq/cryptoassets/tokens";
 
 const getAccountShape: GetAccountShape = async (info) => {
   const { initialAccount, currency, derivationMode } = info;
@@ -24,17 +26,19 @@ const getAccountShape: GetAccountShape = async (info) => {
     derivationMode,
   });
 
+  const subId = "aossdfiofosh";
+
   // get the current account balance state depending your api implementation
-  const { balance } = await getAccount(address);
+  const { balance, energy } = await getAccount(address);
 
   // Merge new operations with the previously synced ones
   const newOperations = await getOperations(accountId, address, startAt);
-  // const VTHOoperations = await getTokenOperations(
-  //   accountId,
-  //   address,
-  //   "0x0000000000000000000000000000456e65726779",
-  //   1
-  // );
+  const VTHOoperations = await getTokenOperations(
+    accountId,
+    address,
+    "0x0000000000000000000000000000456e65726779",
+    1
+  );
   const operations = mergeOps(oldOperations, newOperations);
 
   const shape = {
@@ -44,6 +48,35 @@ const getAccountShape: GetAccountShape = async (info) => {
     spendableBalance: BigNumber(balance),
     operationsCount: operations.length,
     operations: operations,
+    subAccounts: [
+      {
+        type: "TokenAccount" as "TokenAccount",
+        id: `/vechain/vtho/${subId}`,
+        parentId: accountId,
+        token: getTokenById("vechain/vtho"),
+        balance: BigNumber(energy),
+        spendableBalance: BigNumber(energy),
+        creationDate:
+          (initialAccount?.subAccounts &&
+            initialAccount.subAccounts[0]?.creationDate) ||
+          new Date(),
+        operationsCount: VTHOoperations.length,
+        operations: VTHOoperations,
+        pendingOperations:
+          (initialAccount?.subAccounts &&
+            initialAccount.subAccounts[0]?.pendingOperations) ||
+          [],
+        starred:
+          (initialAccount?.subAccounts &&
+            initialAccount.subAccounts[0]?.starred) ||
+          false,
+        balanceHistoryCache:
+          (initialAccount?.subAccounts &&
+            initialAccount.subAccounts[0]?.balanceHistoryCache) ||
+          emptyHistoryCache,
+        swapHistory: [],
+      },
+    ],
   };
 
   return { ...shape, operations };
---
+++ libs/ledgerjs/packages/cryptoassets/src/data/vip180.ts
@@ -0,0 +1,13 @@
+export type vip180Token = [string, string, string, number, boolean?];
+
+const vechainTokens: vip180Token[] = [
+  [
+    "VTHO",
+    "VechainThor",
+    "0x0000000000000000000000000000456E65726779",
+    18,
+    true,
+  ],
+];
+
+export default vechainTokens;
