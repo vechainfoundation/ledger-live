2ac9e5fb875309588214d6617f3c28e7186035f1
Mirko Genoni <mirko.genoni@mail.polimi.it>
2023-02-27 17:24:18 +0100

    add(libs): add cli transaction broadcast

--- libs/ledger-live-common/src/generated/cli-transaction.ts
+++ libs/ledger-live-common/src/generated/cli-transaction.ts
@@ -15,6 +15,7 @@ import solana from "../families/solana/cli-transaction";
 import stellar from "../families/stellar/cli-transaction";
 import tezos from "../families/tezos/cli-transaction";
 import tron from "../families/tron/cli-transaction";
+import vechain from "../families/vechain/cli-transaction";
 
 export default {
   algorand,
@@ -34,4 +35,5 @@ export default {
   stellar,
   tezos,
   tron,
+  vechain,
 };
